13539179219d3af972ef7fa3d9eef9a707fcd317
Ramit Basyal <basyalramit02@gmail.com>
2026-03-20 23:06:37 +0545

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -26,6 +26,7 @@ const Navbar = () => {
         toast.success("Logout Successfully")
       } catch (error) {
         console.log(error.message)
+        dispatch(setUserData(null))
         toast.error(error.response.data.message)        
        }
    }
